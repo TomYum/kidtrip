cc4115c5f0d8dfae2c5ee958148e7356d22f0598
tomyum <hart2005@gmail.com>
2016-04-22 16:12:02 +0000

    Add 'point' model

--- app/Point.php
+++ app/Point.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Point extends Model
 {
     //
+    
+    public function scopeNearPoint($query,Point $point,$distination)
+    {
+        return $query;
+    }
+    
+    public function scopePointsInSquer($query, Point $first_point, Point $second_point)
+    {
+        $fp_lat = $first_point->getLatitude();
+        $fp_lon = $first_point->getLongitude();
+        
+        $sp_lat = $second_point->getLatitude();
+        $sp_lon = $second_point->getLongitude();
+        
+        if ($fp_lat > $sp_lat){
+            
+        }
+        
+        return $query;
+    }
 }
